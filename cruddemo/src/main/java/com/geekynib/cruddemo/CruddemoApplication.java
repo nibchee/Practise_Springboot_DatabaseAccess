b14e04c24cfac6827b2821fc8024a98773a5ff42
nibchee <nirbhaybansal28@gmail.com>
2023-03-18 21:02:52 +0530

    Students saved

--- cruddemo/src/main/java/com/geekynib/cruddemo/CruddemoApplication.java
+++ cruddemo/src/main/java/com/geekynib/cruddemo/CruddemoApplication.java
@@ -1,5 +1,7 @@
 package com.geekynib.cruddemo;
 
+import com.geekynib.cruddemo.dao.StudentDAO;
+import com.geekynib.cruddemo.entity.Student;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -14,11 +16,38 @@ public class CruddemoApplication {
 
 	//Creating Bean this is from Spring Framework executed after beans are loaded
 	@Bean
-	public CommandLineRunner commandLineRunner(String args[]){
+	public CommandLineRunner commandLineRunner(StudentDAO studentDAO){
 		//Java Lamda Expression
 		return  runner->{
-			System.out.println("Hello World");
+		//createStudent(studentDAO);
+		createMultipleStudents(studentDAO);
 		};
+	}
+
+	private void createMultipleStudents(StudentDAO studentDAO) {
+	  //create Multiple Students
+		System.out.println("Creating 3 Student object...");
+		Student tempStudent1=new Student("Nirbhay","Bansal","nirbhaybansal28@gmail.com");
+		Student tempStudent2=new Student("Aayush","Bansal","aayush28@gmail.com");
+		Student tempStudent3=new Student("Aayushi","Bansal","aayushibansal28@gmail.com");
+		//save the student object
+		System.out.println("Saviing the Studets..");
+		studentDAO.save(tempStudent1);
+		studentDAO.save(tempStudent2);
+		studentDAO.save(tempStudent3);
+
+	}
+
+	private void createStudent(StudentDAO studentDAO) {
+		//create Student Object
+		System.out.println("Creating new Student object...");
+		Student tempStudent=new Student("Nirbhay","Bansal","nirbhaybansal28@gmail.com");
+		//save the student object
+		System.out.println("Saving the student...");
+		studentDAO.save(tempStudent);
+
+		//display id of saved student
+		System.out.println("Saved student .Generatd id:"+tempStudent.getId());
 	}
 
 }
