73c14dd15facc52eab2bc66b7a81fdb9d2df03ba
nibchee <nirbhaybansal28@gmail.com>
2023-03-18 18:33:21 +0530

    Command Line Runner added & logging level prioperty

--- cruddemo/src/main/java/com/geekynib/cruddemo/CruddemoApplication.java
+++ cruddemo/src/main/java/com/geekynib/cruddemo/CruddemoApplication.java
@@ -1,13 +1,24 @@
 package com.geekynib.cruddemo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CruddemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CruddemoApplication.class, args);
+	}
+
+	//Creating Bean this is from Spring Framework executed after beans are loaded
+	@Bean
+	public CommandLineRunner commandLineRunner(String args[]){
+		//Java Lamda Expression
+		return  runner->{
+			System.out.println("Hello World");
+		};
 	}
 
 }
